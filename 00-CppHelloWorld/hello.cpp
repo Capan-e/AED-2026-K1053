3b1cd9aa37ec327ca57acec583b5c4ce2aac4a4e
Capan-e <estebancapandegui@gmail.com>
2026-04-25 15:46:23 -0300

    Add files via upload

--- 00-CppHelloWorld/hello.cpp
+++ 00-CppHelloWorld/hello.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
 int main(){
-    std::cout << "Hello World!";
+    std::cout << "Hello World!\n";
+    std::cout << "Esteban Capandegui\n";
+    std::cout << "K1053\n";
+    std::cout << "GRUPO 1";
 }
